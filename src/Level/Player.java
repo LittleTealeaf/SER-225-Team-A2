fa553d72dc1cd8eabd7c13c2b27a024e0e89eab2
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2021-11-02 19:17:10 -0400

    Located Code

--- src/Level/Player.java
+++ src/Level/Player.java
@@ -404,6 +404,7 @@ public abstract class Player extends GameObject {
         }
     }
 
+    //TODO make dynamic to level
     @Override
     public void onEndCollisionCheckX(boolean hasCollided, Direction direction) {
     	// if the player collides with the coordinates specified below, it either stops (beginning of level) or goes back to the start (end of level)
