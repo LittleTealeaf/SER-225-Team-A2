61ed2a4d9f722b45cad0c6748602f50b4854b9ea
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2021-11-03 12:13:38 -0400

    Completely removed the lockedKeys Set in KeyLocker

--- src/Engine/KeyLocker.java
+++ src/Engine/KeyLocker.java
@@ -1,45 +1,19 @@
 package Engine;
 
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.Set;
 
 // This class can be used to keep track of "locked" and "unlocked" keys based on the class
 // For example, it's often useful to "lock" a key if pressed down until its been released, since holding down a key will continually count as a "key press".
 // This way, that "key press" will only be counted once per press.
 // This class does NOT do anything to the keyboard class to prevent a key from actually being detected -- that is not advisable as multiple classes may be detecting key presses separately
 public class KeyLocker {
-    private HashSet<Key> lockedKeys = new HashSet<>();
-    private HashSet<KeyboardAction> lockedAdapters = new HashSet<>();
+    private Set<KeyboardAction> lockedAdapters = new HashSet<>();
 
-    // lock a key
-    public void lockKey(Key... keys) {
-        for(Key key : keys) {
-            lockedKeys.add(key);
-        }
-    }
-
-    // unlock a key
-    public void unlockKey(Key... keys) {
-        for(Key key : keys) {
-            lockedKeys.remove(key);
-        }
-    }
-
-    public void setKeys(boolean locked, KeyboardAction keyChannel) {
-        setKeys(locked,keyChannel.getKeys());
-    }
 
     public void setKeys(KeyboardAction... adapters) {
-        for(KeyboardAction adapter : adapters) {
-            lockedAdapters.add(adapter);
-        }
-    }
-
-    public void setKeys(boolean locked, Key... keys) {
-        if(locked) {
-            lockKey(keys);
-        } else {
-            unlockKey(keys);
-        }
+        Collections.addAll(lockedAdapters, adapters);
     }
 
     public void setAction(KeyboardAction... keyboardActions) {
@@ -59,23 +33,14 @@ public class KeyLocker {
     }
 
     public void lockAction(KeyboardAction... keyboardActions) {
-        for(KeyboardAction keyboardAction : keyboardActions) {
-            lockedAdapters.add(keyboardAction);
-        }
+        Collections.addAll(lockedAdapters, keyboardActions);
     }
 
     public boolean isActionLocked(KeyboardAction keyboardAction) {
         return lockedAdapters.contains(keyboardAction);
     }
 
-
-
-    // check if a key is currently locked
-    public boolean isActionLocked(Key key) {
-        return lockedKeys.contains(key);
-    }
-
     public void clear() {
-        lockedKeys.clear();
+        lockedAdapters.clear();
     }
 }
