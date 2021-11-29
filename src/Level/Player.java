1997304a3efc071c8fd31e2c9bbaacb40b078470
LittleTealeaf <ThomasKwashnak@gmail.com>
2021-11-29 10:32:41 -0500

    Added Comments to Player.java

--- src/Level/Player.java
+++ src/Level/Player.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * Represents the player as it navigates through a map.
  * @author Thomas Kwashnak
  */
 public abstract class Player extends GameObject {
@@ -28,6 +29,7 @@ public abstract class Player extends GameObject {
     private boolean inAir;
     private float absVelocityX, velocityY;
 
+
     public Player(SpriteSheet spriteSheet, float x, float y, String startingAnimationName) {
         super(spriteSheet, x, y, startingAnimationName);
 
@@ -38,16 +40,23 @@ public abstract class Player extends GameObject {
 
     public void update() {
         super.update();
+
+        //Redirects to update depending on player state
         switch (levelState) {
             case PLAYING -> updatePlaying();
             case DEAD -> updateDead();
             case WIN -> updateWin();
         }
 
+        //Updates animation using the player's current playerState
         setCurrentAnimationName(playerState.get(facing));
     }
 
+    /**
+     * Updates the player as whenever it is in the playing state
+     */
     private void updatePlaying() {
+        //Applies gravity
         applyGravity(MAX_FALL_VELOCITY);
 
         //Update Player Action and Direction
@@ -98,41 +107,53 @@ public abstract class Player extends GameObject {
             levelState = LevelState.DEAD;
         }
 
-        inAir = true; //air is decided in moveYHandleCollision()
-        super.moveYHandleCollision(velocityY);
-        super.moveXHandleCollision(absVelocityX * facing.mod);
+        inAir = true; //inAir is updated in collisions
+        //Moves while handling collisions
+        moveYHandleCollision(velocityY);
+        moveXHandleCollision(absVelocityX * facing.mod);
     }
 
+    /**
+     * Updates the player while it is dead
+     */
     private void updateDead() {
-        playerState = PlayerState.DEATH;
-        if (currentFrameIndex > 0) {
+        if (currentFrameIndex > 0) { //Checks if it is not the first frame of the player's death animation
             if (map.getCamera().containsDraw(this)) {
                 applyGravity(MAX_DEATH_FALL_VELOCITY);
             } else for (PlayerListener listener : playerListeners) {
                 listener.onDeath();
             }
-        } else {
+        } else { //Only activates on the first frame of death animation
             velocityY = DEATH_Y_VELOCITY;
         }
+        //Updates Y. Does not use "moveYHandleCollision" since the player can move through the map
         setY(y + velocityY);
     }
 
+    /**
+     * Updates the player after it completes a level
+     */
     private void updateWin() {
-        if (map.getCamera().containsDraw(this)) {
+        if (map.getCamera().containsDraw(this)) { //If the player can seen on the map
             facing = Facing.RIGHT;
-            if (inAir) {
+            if (inAir) { //If the player is still falling
                 playerState = PlayerState.FALL;
                 applyGravity(MAX_FALL_VELOCITY);
                 moveYHandleCollision(velocityY);
-            } else {
+            } else { //Once the player hits the ground, walk to the right
                 playerState = PlayerState.WALK;
-                moveXHandleCollision(walkSpeed);
+                moveX(walkSpeed);
             }
         } else for (PlayerListener listener : playerListeners) {
+            //When the player is off the map, send a onLevelCompleted() to all listeners
             listener.onLevelCompleted();
         }
     }
 
+    /**
+     * Updates the current velocityY with the current gravity
+     * @param maxFallVelocity Gravity Force
+     */
     private void applyGravity(float maxFallVelocity) {
         if (velocityY < maxFallVelocity) {
             velocityY += gravity;
@@ -141,9 +162,9 @@ public abstract class Player extends GameObject {
 
     @Override
     public void onEndCollisionCheckX(boolean hasCollided, Direction direction) {
-        if (hasCollided) {
+        if (hasCollided) { //If it has collided, handles the collision and sets the player's animation to standing
             handleCollision(MapTileCollisionHandler.lastCollidedTileX);
-            if (playerState == PlayerState.WALK) {
+            if (playerState == PlayerState.WALK) { //If the player is walking (aka, not falling), then make the player standing
                 playerState = PlayerState.STAND;
             }
         }
@@ -152,21 +173,23 @@ public abstract class Player extends GameObject {
     @Override
     public void onEndCollisionCheckY(boolean hasCollided, Direction direction) {
         if (hasCollided) {
-            if (direction == Direction.DOWN) {
+            if (direction == Direction.DOWN) { //If direction is down, make inAir false as the player is ontop of something
                 inAir = false;
             }
-            velocityY = 0;
+            velocityY = 0; //Reset velocity
             handleCollision(MapTileCollisionHandler.lastCollidedTileY);
         }
     }
 
     private void handleCollision(MapTile tile) {
         if (tile != null && tile.getTileType() == TileType.LETHAL) {
+            //If the player hits a lethal tile, set its state to dead
             levelState = LevelState.DEAD;
         }
     }
 
     public void hurtPlayer(MapEntity mapEntity) {
+        //Checks the collision type of the entity that collided with the player
         switch (mapEntity.getCollisionType()) {
             case DAMAGE -> PLAYER_HEALTH -= 1;
             case INSTANT_DEATH -> PLAYER_HEALTH = 0;
@@ -174,18 +197,32 @@ public abstract class Player extends GameObject {
         }
     }
 
+    /**
+     * Indicates that the player wins, and starts the winning animation
+     */
     public void completeLevel() {
         levelState = LevelState.WIN;
     }
 
+    /**
+     * @return {@code True} if the player is in the air, {@code false} if the player is on some surface
+     */
     public boolean isInAir() {
         return inAir;
     }
 
+    /**
+     * Adds a listener
+     * @param listener Methods to be checked whenever the player executes an event
+     */
     public void addListener(PlayerListener listener) {
         playerListeners.add(listener);
     }
 
+    /**
+     * Updates the jump height of the player
+     * @param height Velocity to apply when the player jumps
+     */
     public void setJumpHeight(int height) {
         this.jumpHeight = height;
     }
